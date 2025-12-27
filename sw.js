821a0e2015e1af24090bcfd62e51add0a9fa2951
ZAyriaz28 <tu-correo@ejemplo.com>
2025-12-27 00:34:01 -0600

    Mejoras al Sw

--- sw.js
+++ sw.js
@@ -1,4 +1,28 @@
-// sw.js - Service Worker básico
-self.addEventListener('fetch', (event) => {
-    // Este manejador de eventos es obligatorio para que sea instalable
+// sw.js
+const CACHE_NAME = 'wisewallet-v1';
+const assets = [
+  './',
+  './index.html',
+  './dashboard.html',
+  './style.css',
+  './dashboard.js',
+  './manifest.json'
+];
+
+// Instalar el Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Responder desde la caché cuando no hay internet
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
