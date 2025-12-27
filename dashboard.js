a1b5eedfa69eac324f6d214c20a177b531227a38
ZAyriaz28 <tu-correo@ejemplo.com>
2025-12-27 00:27:40 -0600

    Se Agrego un Sw

--- dashboard.js
+++ dashboard.js
@@ -55,3 +55,4 @@ function logout() {
 }
 
 update();
+
---
+++ sw.js
@@ -0,0 +1,4 @@
+// sw.js - Service Worker básico
+self.addEventListener('fetch', (event) => {
+    // Este manejador de eventos es obligatorio para que sea instalable
+});
